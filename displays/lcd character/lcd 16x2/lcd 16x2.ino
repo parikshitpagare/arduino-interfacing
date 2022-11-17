3aa2f1c85961bec3e6da526f7d63316b2de7fb7c
Parikshit Pagare <p.pagare@outlook.in>
2022-11-18 00:18:48 +0530

    updated code

--- displays/lcd character/lcd 16x2/lcd 16x2.ino
+++ displays/lcd character/lcd 16x2/lcd 16x2.ino
@@ -1,9 +1,78 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Interfacing with Character LCD 16x2
+  
+  Author: Parikshit Pagare
+  Date Created: Nov 17, 2022
+*/
 
+#include <LiquidCrystal.h>
+
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+/* Custom characters */
+byte heart[] = {
+  0b00000, 0b01010, 0b10101, 0b10001, 0b01010, 0b00100, 0b00000, 0b00000
+};
+
+byte upArrow[] = {
+    0b00100, 0b01110, 0b10101, 0b00100, 0b00100, 0b00100, 0b00100, 0b00100
+};
+
+byte smiley[] = {
+   0b00000, 0b00000, 0b01010, 0b00000, 0b00000, 0b10001, 0b01110, 0b00000
+};
+
+void setup() {
+  /* Initialize LCD */
+  lcd.begin(16, 2);               
+  
+  /* Create new characters */
+  lcd.createChar(0, upArrow);
+  lcd.createChar(1, heart);
+  lcd.createChar(2, smiley);
+
+  /* Clear dsiplay */
+  lcd.clear();  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  /* Printing text */
+  lcd.print("Arduino is fun!");       
+  delay(5000);
+  lcd.clear();
+  lcd.print("Embedded Systems");      
+  delay(5000);
+  lcd.clear();
+  
+ /* Printing numbers from 0 to 9 */  
+  lcd.setCursor(0, 0);
+  lcd.print("Count 0 to 9");
+  lcd.setCursor(0, 1);  
+  delay(2000);
+  for (int i = 0; i < 10; i++) {
+    lcd.print(i);
+    delay(1000);
+  }
+  delay(5000);
+  lcd.clear();
+  
+  /* Printing custom characters */  
+  lcd.setCursor(0, 0);
+  lcd.print("Custom character");  
+  lcd.setCursor(0, 1);
+  delay(2000);
+  /* When calling lcd.write() must be cast as a byte */
+  lcd.write(byte(0));
+  lcd.print(" ");
+  delay(2000); 
+  lcd.write(byte(1));
+  lcd.print(" ");
+  delay(2000);
+  lcd.write(byte(2));
+  lcd.print(" ");
+  delay(2000);
+  lcd.clear();
 }
+
+
